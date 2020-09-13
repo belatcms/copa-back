e241813ce464de61f74a76441422a91665d12540
Izabela <bebelacampostcms@gmail.com>
2020-09-13 20:17:37 -0300

    ajustando inicializacao

--- CopaApi/Startup.cs
+++ CopaApi/Startup.cs
@@ -24,15 +24,13 @@ namespace CopaApi
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddSingleton<IServicePostEquipe, PostEquipeAPI>();
-            services.AddSingleton<IServiceEquipe, GetEquipeAPI>();
+            services.AddScoped<IServicePostEquipe, PostEquipeAPI>();
+            services.AddScoped<IServiceEquipe, GetEquipeAPI>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
